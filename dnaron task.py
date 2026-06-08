536db0a543c34d8b258a3d8d65a4e24b3bc8c461
tria <tria@email.email.com>
2026-06-08 08:44:18 +0700

    Initial commit: setup git and add number checker

---
+++ dnaron task.py
@@ -0,0 +1,10 @@
+your_number = input ('enter your number.')
+
+your_number = int (your_number)
+
+if your_number % 2 == 0:
+    print (f'{your_number} is even number')
+else:
+    print (f'{your_number} is an odd number.')
+
+# testing git
