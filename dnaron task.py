124f04bc212cd02a8e5ecf094e5c2478f56fc3a6
tria <tria@email.email.com>
2026-06-08 09:32:15 +0700

    added unit test

--- dnaron task.py
+++ dnaron task.py
@@ -1,10 +1,11 @@
-your_number = input ('enter your number.')
+def check_even_odd(your_number):
+    your_number = int(your_number)
+    if your_number % 2 == 0:
+        return f'{your_number} is even number'
+    else:
+        return f'{your_number} is an odd number.'
 
-your_number = int (your_number)
-
-if your_number % 2 == 0:
-    print (f'{your_number} is even number')
-else:
-    print (f'{your_number} is an odd number.')
-
-# testing git
+# This keeps your manual input working normally when you run this file directly
+if __name__ == '__main__':
+    user_input = input('enter your number.')
+    print(check_even_odd(user_input))
